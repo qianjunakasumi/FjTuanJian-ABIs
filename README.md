994432db47543cdecc7032396dbe01d7929b6a2f
千橘 雫霞 <i@qianjunakasumi.ren>
2022-01-07 20:22:38 +0800

    feat✨: 编译指南

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # FjTuanJian-SM4ECB-Go
+
 福建数字化共青团 · 智慧团建之国密 SM4 ECB 模式 ABI - Go
+
+## 编译
+
+开始编译前您需要准备好 NDK 环境并添加其工具链路径至环境变量 `PATH`。
+例如： `$AndroidHOME\Sdk\ndk\23.1.7779620\toolchains\llvm\prebuilt\windows-x86_64\bin\`
+
+默认情况下编译 android31 aarch64 ABI，您可编辑 `build.ps1` 手动修改。
---
+++ build.ps1
@@ -0,0 +1,14 @@
+﻿$Env:GOOS='android'
+$Env:GOARCH='arm64'
+$Env:CGO_ENABLED='1'
+$Env:CC='aarch64-linux-android31-clang.cmd'
+
+go env
+"请确认上述信息，按任意键开始编译"
+
+[Console]::ReadKey() | Out-Null
+"开始编译..."
+
+go build -buildmode='c-shared' -ldflags='-s -w' -o='builds/libsm4.so' main.go
+
+"完成！"
